bd3b9c70e1a5c6a018ece4797ebb1a6a26cd4c7d
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-02-06 12:55:24 +0100

    feat: Modificar nombres de variables
    Modificar algunod nombres del código a palabras de la kata de bowling

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -1,60 +1,59 @@
 class ScoreCard:
-    def __init__(self, score_string):
-        self.rolls = self._parse_rolls(score_string)
-        self.frame_pins = self._build_frames()  
-    def _parse_rolls(self, score_string):
-        rolls = []
-        for char in score_string:
-            if char == 'X':          # strike
-                rolls.append(10)
-            elif char == '-':        # falla la tirada
-                rolls.append(0)
-            elif char == '/':        # semipleno
-                rolls.append(10 - rolls[-1])
-            else:                    # número de pines derribados
-                rolls.append(int(char))
-        return rolls
+    def __init__(self, scorecard):
+        self.throws = self._parse_throws(scorecard)
+        self.frames = self._build_frames()  
+
+    def _parse_throws(self, scorecard):
+        throws = []
+        for symbol in scorecard:
+            if symbol == 'X':          # strike
+                throws.append(10)
+            elif symbol == '-':        # miss / foul
+                throws.append(0)
+            elif symbol == '/':        # spare
+                throws.append(10 - throws[-1])
+            else:                      # pins knocked down
+                throws.append(int(symbol))
+        return throws
 
     def _build_frames(self):
         frames = []
-        i = 0
+        roll = 0
         for frame in range(10):
-            if self.rolls[i] == 10:  # strike
+            if self.throws[roll] == 10:   # strike
                 frames.append([10])
-                i += 1
+                roll += 1
             else:
-                frames.append([self.rolls[i], self.rolls[i+1]])
-                i += 2
-        # Tiradas extra del décimo frame
-        if i < len(self.rolls):
-            frames[-1].extend(self.rolls[i:])
+                frames.append([self.throws[roll], self.throws[roll + 1]])
+                roll += 2
+        # Bonus throws in the 10th frame
+        if roll < len(self.throws):
+            frames[-1].extend(self.throws[roll:])
         return frames
 
     def score(self):
-        # Calcula el puntaje total de la partida.
-        
-        total_score = 0
-        roll_index = 0
+        game_score = 0
+        roll = 0
         for frame in range(10):
-            if self._is_strike(roll_index):
-                total_score += 10 + self._strike_bonus(roll_index)
-                roll_index += 1
-            elif self._is_spare(roll_index):
-                total_score += 10 + self._spare_bonus(roll_index)
-                roll_index += 2
+            if self._is_strike(roll):
+                game_score += 10 + self._strike_bonus(roll)
+                roll += 1
+            elif self._is_spare(roll):
+                game_score += 10 + self._spare_bonus(roll)
+                roll += 2
             else:
-                total_score += self.rolls[roll_index] + self.rolls[roll_index + 1]
-                roll_index += 2
-        return total_score
+                game_score += self.throws[roll] + self.throws[roll + 1]
+                roll += 2
+        return game_score
 
-    def _is_strike(self, roll_index):
-        return self.rolls[roll_index] == 10
+    def _is_strike(self, roll):
+        return self.throws[roll] == 10
 
-    def _is_spare(self, roll_index):
-        return self.rolls[roll_index] + self.rolls[roll_index + 1] == 10
+    def _is_spare(self, roll):
+        return self.throws[roll] + self.throws[roll + 1] == 10
 
-    def _strike_bonus(self, roll_index):
-        return self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
+    def _strike_bonus(self, roll):
+        return self.throws[roll + 1] + self.throws[roll + 2]
 
-    def _spare_bonus(self, roll_index):
-        return self.rolls[roll_index + 2]
+    def _spare_bonus(self, roll):
+        return self.throws[roll + 2]
